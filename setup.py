28841870abbc070b77f34689f0bcbb91b226fc0d
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-09-17 20:55:44 +0200

    Monkey patches xeus-cling to display type information

--- setup.py
+++ setup.py
@@ -4,6 +4,18 @@ import sys
 from setuptools import setup, Extension
 from codecs import open # To open the README file with proper encoding
 from setuptools.command.test import test as TestCommand # for tests
+from setuptools.command.install import install
+
+# Monkey patches xeus-cling to display type information
+import shutil
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+    def run(self):
+        file = "xmime.hpp"
+        target = os.path.join(self.install_base, "include", "xcpp", file)
+        print("manually copying {} to {} ".format(file, target))
+        shutil.copyfile(file, target)
+        install.run(self)
 
 setup(
     name = "info-111",
@@ -20,4 +32,7 @@ setup(
       'Programming Language :: C++',
     ], # classifiers list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
     scripts=['bin/info-111'],
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
 )
---
+++ xmime.hpp
@@ -0,0 +1,36 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille, Loic Gouarin and Sylvain Corlay       *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XCPP_MIME_HPP
+#define XCPP_MIME_HPP
+
+#include "cling/Interpreter/RuntimePrintValue.h"
+#include "xeus/xjson.hpp"
+
+// Patch for info-111: print type information alongside outputs
+#include <cxxabi.h>
+template<class C>
+char * type(C c) {
+    int status;
+    char * demangled;
+    return abi::__cxa_demangle(typeid(c).name(),0,0,&status);
+}
+
+namespace xcpp
+{
+    // Default implementation of mime_bundle_repr
+    template <class T>
+    xeus::xjson mime_bundle_repr(const T& value)
+    {
+        auto bundle = xeus::xjson::object();
+        bundle["text/plain"] = cling::printValue(&value) + "\ntype: "+type(value);
+        return bundle;
+    }
+}
+
+#endif
